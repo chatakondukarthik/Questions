cf4521587ba8cdbfbb15d0046aef860366ec412b
chatakondukarthik <chatakondukarthik24@gmail.com>
2020-10-21 14:48:10 +0530

    Updated one question

--- Codes-Ans/StepsToMakeItZero.py
+++ Codes-Ans/StepsToMakeItZero.py
@@ -4,8 +4,8 @@ num = input("Enter the number please : ")
 counter = 0  # To count the number of processess
 while(num == 0):
     if(num % 2 != 0):
-        num -= 1
+        num = num - 1
     else:
-        num %= 2
-    counter += 1
+        num = num % 2
+    counter = counter + 1
 print(counter)
